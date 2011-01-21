e887adb94525d486d04516f4decf5db2fbe30800
David Zhang <dmonopoly10@gmail.com>
2011-01-21 12:36:03 -0500

    just did p76

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -5,18 +5,32 @@ module Codebreaker
 		end
 		
 		def start(secret)
+			@secret = secret
 			@output.puts 'Welcome to Codebreaker!'
 			@output.puts 'Enter guess:'
 		end
 		
 		def guess(guess)
-		
+			mark = ''
+			(0..3).each do |index|
+				if exact_match?(guess, index)
+					mark << '+'
+				end
+			end
+			(0..3).each do |index|
+				if number_match?(guess, index)
+					mark << '-'
+				end
+			end
+			@output.puts mark
 		end
 		
-		def get_mark(guess) # remove
-			# algorithm here
-			mark = ''
-			@output.puts mark
+		def exact_match?(guess, index)
+			guess[index] == @secret[index]
+		end
+		
+		def number_match?(guess, index)
+			@secret.include?(guess[index])
 		end
 	end
 end
